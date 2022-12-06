781cb52a6cdce092d9adfbc12ffbd2fea9032064
Jonathan Lascano <jonathanlascano2393@gmail.com>
2022-12-05 17:13:45 -0800

    Update ParallaxContainer

--- src/components/ParallaxContainer/ParallaxContainer.jsx
+++ src/components/ParallaxContainer/ParallaxContainer.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import styles from '../ParallaxContainer/ParallaxContainer.module.css';
 
 const ParallaxContainer = ({ image, content }) => {
+  const isMobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  console.log(isMobile);
   return (
-    <div className={styles.parallaxContainer} style={{ backgroundImage: `url(${image})` }}>
+    <div className={!isMobile ? styles.parallaxContainer : styles.removeParallax} style={{ backgroundImage: `url(${image})` }}>
       <div className="topContentContainer">
         {content}
       </div>
